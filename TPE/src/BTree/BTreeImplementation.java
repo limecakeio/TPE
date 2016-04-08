3b5b513c58a3b4978b3058be5a2ece3420452e0a
Richard Vladimirskij <admin@limecake.com.au>
2016-04-08 16:12:43 +0200

    Become the tree.

---
+++ TPE/src/BTree/BTreeImplementation.java
@@ -0,0 +1,14 @@
+package BTree;
+
+public class BTreeImplementation {
+
+	public BTreeImplementation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
