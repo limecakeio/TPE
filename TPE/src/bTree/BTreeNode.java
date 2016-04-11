1670c4c0051148786713e5ad95eb6bca1f917153
SkiveK <pelevin@web.de>
2016-04-11 15:22:16 +0200

    15:22

--- TPE/src/bTree/BTreeNode.java
+++ TPE/src/bTree/BTreeNode.java
@@ -2,6 +2,51 @@ package bTree;
 
 public class BTreeNode {
 
-	Integer [] values;
+	private Integer [] values;
+	private BTreeNode [] children;
+
+	// SETUP BTREE NODE
+	BTreeNode(int magnitude){
+
+		// array length: (magnitude x2)+1
+		values = new Integer [(magnitude*2)+1];
+		children = new BTreeNode [(magnitude*2)+1];
+		
+		setChildren(children);
+		
+	}
+
+	BTreeNode(int magnitude, Integer value){
+
+		// array length: (magnitude x2)+1
+		values = new Integer [(magnitude*2)+1];
+		children = new BTreeNode [(magnitude*2)+1];
+		
+		// set values
+		values[0] = value;
+		setChildren(children);
+	}
+
+	// BTREE NODE MTHODS
+	public void setValues(Integer value, int index){
+		values [index] = value;
+	}
+
+	public Integer getValues(int index){
+		return values[index];
+	}
 	
+	public BTreeNode getChild(int index){
+		return children[index];
+	}
+	
+	// SPECIAL
+	private BTreeNode[] setChildren(BTreeNode[] children){
+		
+		for(int i = 0; i < children.length; i++){
+			children [i] = null;
+		}
+		return children;
+	}
+
 }
