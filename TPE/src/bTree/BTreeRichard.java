c033bf1cce6e4c4ea08c63f3df411c4606b45d45
Richard Vladimirskij <admin@limecake.com.au>
2016-04-18 19:45:03 +0200

    First attempt at burstLeaf...

--- TPE/src/bTree/BTreeRichard.java
+++ TPE/src/bTree/BTreeRichard.java
@@ -1,5 +1,31 @@
 package bTree;
 
 public class BTreeRichard {
-	
+	public void burstTreeLeaf(int magnitude, BTreeNode parent, BTreeNode brokenLeaf) {
+		
+		//Arrange a new node for all elements > magnitude
+		BTreeNode newLeaf = new BTreeNode(magnitude);
+		
+		//Insert elements into the new node and remove from source
+		for(int i = magnitude+1; i < magnitude*2; i++) {
+			newLeaf.setValues(brokenLeaf.getValue(i), i-magnitude+1);
+			brokenLeaf.setValues(null, i);
+		}
+		
+		//Placing middle-value into parent-node
+		Integer mValue = brokenLeaf.getValue(magnitude);
+		brokenLeaf.setValues(null, magnitude); // Reset middle-value
+		
+		int pos = 0;
+		for(int i = 0; i < magnitude; i++) {
+			if(parent.getChild(i) != null)
+				pos += i++;
+		}
+		parent.setValues(mValue, pos);
+		
+		Integer.insertionSort(parent.getValues());
+		
+		criteriaCheck(parent);
+		
+	}
 	}
