c6811ea712198608ccfce90cc802f430b22d3ce6
SkiveK <pelevin@web.de>
2016-04-18 17:58:07 +0200

    --

--- TPE/src/bTree/BTree.java
+++ TPE/src/bTree/BTree.java
@@ -1,31 +1,31 @@
 package bTree;
 
 public class BTree implements BTreeInterface{
-	
+
 	private int magnitude;
 	private BTreeNode root;
-	
+
 	// B-TREE SET UP
-	
+
 	public BTree(int magnitude){
 		root = null;
 		this.magnitude = magnitude;
 	}
-	
+
 	public BTree(Integer o, int magnitude){
 		root = new BTreeNode(o, magnitude);
 		this.magnitude = magnitude;
 	}
-	
+
 	// CORE INTERFACE METHODS
 	public boolean insert(Integer o){
-		
+
 		// -- 1 -- 
 		preinsert(o);
-		
+
 		return false;
 	}
-	
+
 	public boolean insert(String filename) {
 		// TODO Auto-generated method stub
 		return false;
@@ -55,7 +55,7 @@ public class BTree implements BTreeInterface{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public boolean isEmpty(){
 		return root == null;
 	}
@@ -86,23 +86,23 @@ public class BTree implements BTreeInterface{
 	 * While using the postorder transversion strategy, we first attempt to look up and enter
 	 * all of the children nodes referenced within the given node before entering the given node itself.
 	 */
-	
+
 	public void printPreorder(){
-		
+
 		BTreeNode pointer = root;
 		printPreorder(pointer);
 	}
 	private void printPreorder(BTreeNode pointer){
 		if (pointer != null){
-			
+
 			// get data: current node
 			Integer[] values = pointer.getValues();
 			BTreeNode[] children = pointer.getChildren();
-			
+
 			// first: get all current node values 
 			for (int i = 0; i < values.length -1; i++){
 				if (values[i] != null)
-				System.out.print(Integer.toString(values[i]) + ", ");
+					System.out.print(Integer.toString(values[i]) + ", ");
 			}
 			// second: repeat for each child of the node
 			for (int i = 0; i < children.length -1; i++){
@@ -112,28 +112,28 @@ public class BTree implements BTreeInterface{
 	}
 
 	public void printInorder(){
-		
+
 		BTreeNode pointer = root;
 		printInorder(pointer);
 	}
 	private void printInorder(BTreeNode pointer){
 		if (pointer != null){
-			
+
 			// get data: current node
 			Integer[] values = pointer.getValues();
 			BTreeNode[] children = pointer.getChildren();
-			
+
 			// first: visit left-most node
 			printInorder(pointer.getChild(0));
-			
+
 			// second: visit smallest parent node value
 			if (values[0] != null){
-			System.out.print(values[0] + ", ");
+				System.out.print(values[0] + ", ");
 			}
-			
+
 			// third: repeat strategy for bigger elements
 			for (int i = 1; i < children.length -1; i++){
-				
+
 				// get remaining values of the node
 				if (values[i] != null){
 					System.out.print(values[i] + ", ");
@@ -145,17 +145,17 @@ public class BTree implements BTreeInterface{
 	}
 
 	public void printPostorder(){
-		
+
 		BTreeNode pointer = root;
 		printPostorder(pointer);
 	}
 	private void printPostorder(BTreeNode pointer){
 		if (pointer != null){
-			
+
 			// get data: current node
 			Integer[] values = pointer.getValues();
 			BTreeNode[] children = pointer.getChildren();
-			
+
 			// first: try to enter all children nodes
 			for (int i = 0; i < children.length -1; i++){
 				printPostorder(pointer.getChild(i));
@@ -163,31 +163,31 @@ public class BTree implements BTreeInterface{
 			// second: get all current node values
 			for (int i = 0; i < values.length -1; i++){
 				if (values[i] != null)
-				System.out.print(Integer.toString(values[i]) + ", ");
+					System.out.print(Integer.toString(values[i]) + ", ");
 			}
 		}
 	}
 
 	public void printLevelorder() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	// SPECIAL METHODS
-	
+
 	// public for testing
 	public boolean preinsert(Integer o){
-		
+
 		int i = 0;
 		int child = 0;
-		int neighborLeaf = -1;
+		int neighbourLeaf = -1;
 		boolean success = false;
 		BTreeNode parent = null;
 		BTreeNode pointer = root;
 
 		// tree contains elements
 		if (pointer != null){
-			
+
 			// search and insert
 			while (!success){
 
@@ -195,7 +195,7 @@ public class BTree implements BTreeInterface{
 				if (pointer.getValue(i) == null){
 					pointer.setValues(o, i);
 					Integer.insertionSort(pointer.getValues());
-					
+
 					// check node: healthy
 					if (criteriaCheck(pointer)){
 						success = true;
@@ -204,21 +204,29 @@ public class BTree implements BTreeInterface{
 					else {
 						// case: root
 						if (parent == null){
-							 burstTree(magnitude, pointer);
-							 success = true;
+							burstTree(magnitude, pointer);
+							success = true;
 						}
 						// case: leaf
 						else {
 							// first step: check neighbor leaf
-							neighborLeaf = leafCheck(parent, child);
-							
-						}
+							neighbourLeaf = leafCheck(parent, child);
 
+							// neighbour leaves have free storage: re-arrange values 
+							if (neighbourLeaf != -1){
+								rebalance(pointer, parent, child, neighbourLeaf);
+								success = true;
+							}
+							// neighbor leaves are full: burst the leaf
+							else {
+								// BOOOOM!!!
+							}
+						}
 					}
 				}
 				// check stored value: o > value[i]
 				else if ((pointer.getValue(i)).compareTo(o) == 1){
-					
+
 					// check right child: not present
 					if (pointer.getChild(i+1) == null){
 						i++;
@@ -262,46 +270,46 @@ public class BTree implements BTreeInterface{
 		// testing
 		return success;
 	}
-	
+
 	private void burstTree(int magnitude, BTreeNode node){
 		//Split the root - create 2 new nodes and populate
-				BTreeNode newRoot = new BTreeNode(magnitude);
-				BTreeNode rightChild = new BTreeNode(magnitude);
-				
-				//Set new root
-				newRoot.setValues(node.getValue(magnitude), 0);
-				node.setValues(null, magnitude);
-				
-				//Fill the right node
-				for(int i = magnitude+1; i < node.getValues().length; i++) {
-					rightChild.setValues(node.getValue(i), i-magnitude+1);
-					System.out.println("R: " + Integer.toString(rightChild.getValue(i-magnitude+1)));
-					node.setValues(null, i);
-				}
+		BTreeNode newRoot = new BTreeNode(magnitude);
+		BTreeNode rightChild = new BTreeNode(magnitude);
 
-				//Set new references
-				newRoot.setChild(node, 0);
-				newRoot.setChild(rightChild, 1);
-				
-				// set newroot as actual root (duuuh)
-				root = newRoot;
+		//Set new root
+		newRoot.setValues(node.getValue(magnitude), 0);
+		node.setValues(null, magnitude);
+
+		//Fill the right node
+		for(int i = magnitude+1; i < node.getValues().length; i++) {
+			rightChild.setValues(node.getValue(i), i-magnitude+1);
+			System.out.println("R: " + Integer.toString(rightChild.getValue(i-magnitude+1)));
+			node.setValues(null, i);
+		}
+
+		//Set new references
+		newRoot.setChild(node, 0);
+		newRoot.setChild(rightChild, 1);
+
+		// set newroot as actual root (duuuh)
+		root = newRoot;
 	}
-	
+
 
 	private boolean criteriaCheck(BTreeNode pointer){
 		Integer [] values = pointer.getValues();
-		
+
 		// check last array element
 		if (values[values.length-1] == null)
 			return true;
 		return false;
 	}
-	
+
 	private int leafCheck(BTreeNode parent, int child){
-		
+
 		int i = child-1;
 		BTreeNode[] children = parent.getChildren();
-		
+
 		while (i > 0){
 			// get values: current child
 			Integer[] values = children[i].getValues();
@@ -312,6 +320,38 @@ public class BTree implements BTreeInterface{
 			i--;
 		}
 		return -1;
-		
 	}
+
+	private void rebalance(BTreeNode pointer, BTreeNode parent, int child, int neighbourLeaf){
+
+		for (int i = neighbourLeaf; i <= child; i++){
+
+			boolean set = false;
+			Integer parentValue = parent.getValue(i);
+			Integer childValue = parent.getChild(i+1).getValue(0);
+
+
+			for (int j = 0; j < pointer.getValues().length-1 && !set; j++){
+				if (parent.getChild(i).getValue(j+1) == null){
+
+					// search and set position for parent value (from the top to the left) 
+					parent.getChild(i).setValues(parentValue, Integer.transformInteger(parent.getChild(i).getValue(j+1)));
+					set = true;
+				}
+			}
+			// place child value inside parent node (from the left to the top)
+			parent.setValues(childValue, i);
+			//Integer.insertionSort(parent.getChild(i+1).getValues());
+			
+			// sort away: first 'null' value
+			for (int k = 0; k < pointer.getValues().length-2; k++){
+				Integer x = parent.getChild(i+1).getValue(k);
+				Integer y = parent.getChild(i+1).getValue(k+1);
+				parent.getChild(i+1).setValues(y, k);
+				parent.getChild(i+1).setValues(x, k+1);
+			}
+		}		
+	}
+
+
 }
