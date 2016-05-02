dfbc0692b6d2763cef13be0877f002872319f559
SkiveK <pelevin@web.de>
2016-05-02 15:56:16 +0200

    ice-cream, maddafakka.

---
+++ TPE/TPE/eisDiele/Eis.java
@@ -0,0 +1,27 @@
+package eisDiele;
+
+public class Eis {
+
+	// core data: ice-cream
+	private double preis;
+	private String art;
+	private String name;
+	private String behaeltnis;
+	
+	
+	private String[] sorten;
+	private String[] extras;
+	
+	public void vorbereiten(){
+		// TODO
+	}
+	
+	public void fuellen(){
+		// TODO
+	}
+	
+	public void dekorieren(){
+		// TODO
+	}
+	
+}
---
+++ TPE/TPE/eisDiele/Eisdiele.java
@@ -0,0 +1,28 @@
+package eisDiele;
+
+public class Eisdiele {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+	// ESSENTIAL METHODS
+	void begruessen(){
+		// TODO
+	}
+	
+	void kassieren(){
+		// TODO
+	}
+	
+	void verabschieden(){
+		// TODO
+	}
+	
+	void entschuldigen(){
+		// TODO
+	}
+
+}
