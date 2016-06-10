c6ffa1db9cbf58eb54f3b464bdce55efbc259bb3
Richard Vladimirskij <admin@limecake.com.au>
2016-06-10 13:20:41 +0200

    Ringpuffa

--- TPE/TPE/ringPuffer/Ringpuffer.java
+++ TPE/TPE/ringPuffer/Ringpuffer.java
@@ -1,66 +1,79 @@
 package ringPuffer;
 
-public class Ringpuffer extends Thread {
+public class Ringpuffer {
+
 private Object[] rp;
-private boolean ready;
-private int in, out, size;
+private int in = 0, out = 0;
 private int inRuns = 0;
 private int outRuns = 0;
+private int time;
+private TimerThread t;
+private ConsumerThread[] ct;
+private ProducerThread[] pt;
 
-Ringpuffer() {}
 
-Ringpuffer(int size) {
-	this.size = size;
+Ringpuffer(int size, int cons, int prods, int time) {
 	rp = new Object[size];
-	ready = true;
-	in = 0;
-	out = 0;
-}
-
-/**
- * */
-public void setReady(boolean val) {
-	ready = val;
-}
-
-public boolean getReady() {
-	return ready;
+	ct = new ConsumerThread[cons];
+	pt = new ProducerThread[prods];
+	this.time = time;
 }
 
 /**
  * Returns the first object in the queue.
  * @throws RingpufferException if the queue is full or if it is empty.
  * */
-public Object get() throws RingpufferException {
-	Object val;
+public synchronized Object get() throws InterruptedException {
+	Object val = new Object();
 
-	//Ringpuffer is empty
-	if(rp[out] == null || (inRuns <= outRuns && out == in))
-		throw new RingpufferException("Ringbuffer is empty");
+	//If consumer wants to over take producer it has to wait.
+	while(rp[out] == null || (inRuns <= outRuns && out == in)) {
+		wait();
+	}
 		
 	val = rp[out];
+	
 	out = (out+1) % rp.length;
 	
 	if(out == 0)
 		outRuns++;
 	
+	notifyAll();
+	
 	return val;
 }
 
-/**Adds a new Object to the end of the queue
- * @throws RingpufferException if the Ringpuffer is full */
-public void put(Object o) throws RingpufferException {
+/**Adds a new Object to the end of the queue and overwrites already read ones.
+ * @throws InterruptedException if the thread was interrupted. */
+public synchronized void put(Object o) throws InterruptedException {
 	
-	//Ringbuffer is full
-	if(inRuns > outRuns && in == out)
-			throw new RingpufferException("Ringbuffer is full");
+	//Producer is always ahead of of consumer, so it cannot lap the other.
+	while(inRuns > outRuns && in == out)
+		wait();
 	
 	rp[in] = o;
 	
-	in = (in+1) % rp.length;
+	in = (in+1) % rp.length;	
 	
 	if(in == 0)
-		inRuns++;	
+		inRuns++;
+	
+	notifyAll();
+}
+
+public void setConsumer(int pos, int sleep, String name) {
+	ct[pos] = new ConsumerThread(this, t, sleep, name);
+}
+
+public void setProducer(int pos, int sleep, String name) {
+	pt[pos] = new ProducerThread(this, t, sleep, name);
+}
+
+public void startTimer() {
+	//TODO Turn seconds into minutes
+	t = new TimerThread(time);
+	t.start();
+	System.out.println("Timer has been started with a " + time + " countdown.");
 }
 
 }
--- TPE/TPE/ringPuffer/RingpufferTest.java
+++ TPE/TPE/ringPuffer/RingpufferTest.java
@@ -6,96 +6,95 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RingpufferTest {
-	Ringpuffer r1 = new Ringpuffer();
-	Ringpuffer r2 = new Ringpuffer();
-	Ringpuffer r3 = new Ringpuffer();
+	Ringpuffer r1;
+	Ringpuffer r2;
+	Ringpuffer r3;
 
 	@Before
 	public void setUp() throws Exception {
-		r1.put(new Integer(23));
-		r1.put(new Integer(55));
-		r1.put(new Integer(12));
-		r1.put(new Integer(32));
-		r1.put(new Integer(27));
-		
-		r3.put(new Integer(23));
-		r3.put(new Integer(55));
-		r3.put(new Integer(12));
-	}
-
-	@Test(expected = RingpufferException.class)
-	public void testGetEmpty() throws RingpufferException {
-				r2.get();
+		r1 = new Ringpuffer(3, 2, 3, 5);
 	}
 	
 	@Test
-	public void testGetFirst() throws RingpufferException {
-		String result = "";
-		result = r1.get() + "";
-		assertEquals("23", result);
+	public void testConsumerSlowerProducerFaster() throws InterruptedException {
+		r1.startTimer();
+		r1.setConsumer(0, 2000, "Consumer 1");
+		r1.setConsumer(1, 3000, "Consumer 2");
+		r1.setProducer(0, 1000, "Producer 1");
+		r1.setProducer(1, 750, "Producer 2");
+		r1.setProducer(2, 500, "Producer 3");
 	}
 	
 	@Test
-	public void testGetAll() {
-		String result = "";
-		try {
-			for(int i = 0; i < 5; i++)
-				result += r1.get() + " ";
-		} catch (RingpufferException e) {
-		}
-		assertEquals("23 55 12 32 27 ", result);
-	}
-	
-	@Test(expected = RingpufferException.class)
-	public void testGetOneTooMany() throws RingpufferException {
-			for(int i = 0; i <= 5; i++)
-				r1.get();
-	}
-	
-	@Test(expected = RingpufferException.class)
-	public void testPutOneTooMany() throws RingpufferException {
-			r1.put(new Integer(66));
+	public void testProducerSlowerConsumerFaster() throws InterruptedException {
+		r1.startTimer();
+		r1.setConsumer(0, 750, "Consumer 1");
+		r1.setConsumer(1, 500, "Consumer 2");
+		r1.setProducer(0, 1000, "Producer 1");
+		r1.setProducer(1, 2000, "Producer 2");
+		r1.setProducer(2, 3000, "Producer 3");	
 	}
 	
 	@Test
-	public void testRead1Add1() throws RingpufferException {
-		r1.get();
-		r1.put(new Integer(66));
-		String result = r1.get() + "";
-		assertEquals("55", result);
-		r1.get();
-		r1.get();
-		r1.get();
-		result = r1.get() + "";
-		assertEquals("66", result);	
+	public void testProducerConsumerEqualTime() throws InterruptedException {
+		r1.startTimer();
+		r1.setConsumer(0, 1000, "Consumer 1");
+		r1.setConsumer(1, 1000, "Consumer 2");
+		r1.setProducer(0, 1000, "Producer 1");
+		r1.setProducer(1, 1000, "Producer 2");
+		r1.setProducer(2, 1000, "Producer 3");	
 	}
-	
-	@Test(expected = RingpufferException.class)
-	public void testFillHalfReadHalfPlusOne() throws RingpufferException {
-		r3.get();
-		r3.get();
-		r3.get();
-		r3.get();
-	}
-	
-	@Test
-	public void testAlternatingFillAndRead() throws RingpufferException {
-		r3.get();
-		r3.get();
-		r3.put(22);
-		r3.put(99);
-		r3.get();
-		r3.get();
-		r3.put(1);
-		r3.put(2);
-		r3.put(3);
-		r3.put(4);
-		r3.get();
-		r3.get();
-		r3.get();
-		r3.get();
-		String result = r3.get() +"";
-		assertEquals("4", result);
-	}
+//	
+//	@Test(expected = RingpufferException.class)
+//	public void testGetOneTooMany() throws RingpufferException, InterruptedException {
+//			for(int i = 0; i <= 5; i++)
+//				r1.get();
+//	}
+//	
+//	@Test(expected = RingpufferException.class)
+//	public void testPutOneTooMany() throws RingpufferException, InterruptedException {
+//			r1.put(new Integer(66));
+//	}
+//	
+//	@Test
+//	public void testRead1Add1() throws RingpufferException, InterruptedException {
+//		r1.get();
+//		r1.put(new Integer(66));
+//		String result = r1.get() + "";
+//		assertEquals("55", result);
+//		r1.get();
+//		r1.get();
+//		r1.get();
+//		result = r1.get() + "";
+//		assertEquals("66", result);	
+//	}
+//	
+//	@Test(expected = RingpufferException.class)
+//	public void testFillHalfReadHalfPlusOne() throws RingpufferException, InterruptedException {
+//		r3.get();
+//		r3.get();
+//		r3.get();
+//		r3.get();
+//	}
+//	
+//	@Test
+//	public void testAlternatingFillAndRead() throws RingpufferException, InterruptedException {
+//		r3.get();
+//		r3.get();
+//		r3.put(22);
+//		r3.put(99);
+//		r3.get();
+//		r3.get();
+//		r3.put(1);
+//		r3.put(2);
+//		r3.put(3);
+//		r3.put(4);
+//		r3.get();
+//		r3.get();
+//		r3.get();
+//		r3.get();
+//		String result = r3.get() +"";
+//		assertEquals("4", result);
+//	}
 
 }
